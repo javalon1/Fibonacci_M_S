0447f953784c92acc463eef626916c52acf5f5d3
Michal Stusik <x5htpx@gmail.com>
2019-05-12 11:25:31 +0100

    recursive solution 2

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,29 +1,61 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static int fibonacci(int n){
-        if (n<=2){
+public class App {
+    public static int fibonacci(int n) {
+        if (n <= 2) {
             return 1;
-        }else{
-            return fibonacci(n-2) + fibonacci(n-1);
+        } else {
+            return fibonacci(n - 2) + fibonacci(n - 1);
         }
     }
 
+    public static int fibonacci2() {
 
-    public static void main( String[] args )
-    {
-        System.out.println(fibonacci(5));
-        int tab[] = new int[100];
-        tab[0]=0;
-        tab[1]=1;
-        for (int i=2; i<tab.length; i++){
-            tab[i]= tab[i-1] + tab[i-2];
-            System.out.println(tab[i]);
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int A = 0;
+        int B = 1;
+        int C = 1;
+        int K = 2;
+
+        if (N < 2) {
+return N;
+            
+
+        } else {
+
+
+            while (K < N) {
+                A = B = C = A + B;
+
+                K++;
+
+            }
+
+        }
+System.out.println(C);
+return C;
+
     }
+
+
+
+
+        public static void main (String[]args )
+        {
+            System.out.println(fibonacci(5));
+            int tab[] = new int[100];
+            tab[0] = 0;
+            tab[1] = 1;
+            for (int i = 2; i < tab.length; i++) {
+                tab[i] = tab[i - 1] + tab[i - 2];
+                System.out.println(tab[i]);
+            }
+        }
     }
-}
